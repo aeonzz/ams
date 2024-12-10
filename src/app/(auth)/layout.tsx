5b3593bbb06e60352367993aca70d6b207716fb7
unknown <christiancaneos1@gmail.com>
2024-12-10 14:16:21 +0800

    asf

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -11,9 +11,9 @@ export default async function AuthLayout({
 
   return (
     <div className="relative flex min-h-screen flex-col items-center justify-center overflow-hidden px-4">
-      <div className="absolute -left-16 -top-[95px] h-[150px] w-[800px] -rotate-[15deg] overflow-hidden border-8 border-primary bg-yellow"></div>
+      {/* <div className="absolute -left-16 -top-[95px] h-[150px] w-[800px] -rotate-[15deg] overflow-hidden border-8 border-primary bg-yellow"></div>
 
-      <div className="absolute -bottom-[95px] -right-16 h-[150px] w-[800px] -rotate-[15deg] overflow-hidden border-8 border-primary bg-yellow"></div>
+      <div className="absolute -bottom-[95px] -right-16 h-[150px] w-[800px] -rotate-[15deg] overflow-hidden border-8 border-primary bg-yellow"></div> */}
 
       <div className="relative z-10 w-full max-w-md">{children}</div>
     </div>
